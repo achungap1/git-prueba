35dc62933dfae6a4e897e62fd193836649aa3434
LuAr <ingacp_90@hotmail.com>
2022-10-21 12:21:14 -0500

    se agrego hola mundo

--- src/proyectoprueba/Proyectoprueba.java
+++ src/proyectoprueba/Proyectoprueba.java
@@ -7,7 +7,7 @@ public class Proyectoprueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola Arturo");
     }
     
 }
