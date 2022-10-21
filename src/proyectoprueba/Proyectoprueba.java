9bfc6c5d895facfd05d60367008649cc22fe8a90
LuAr <ingacp_90@hotmail.com>
2022-10-21 12:06:27 -0500

    primer commit

---
+++ src/proyectoprueba/Proyectoprueba.java
@@ -0,0 +1,13 @@
+
+package proyectoprueba;
+/*Esto es un proyecto de prueba*/
+public class Proyectoprueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
